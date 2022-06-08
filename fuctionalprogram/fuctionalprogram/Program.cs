3314715d1f903846b2d48f52094f0d182335a863
guri97 <guri84096@gmail.com>
2022-06-08 20:31:04 +0530

    [gurpreet] Swaping_Two_Numbers

--- fuctionalprogram/fuctionalprogram/Program.cs
+++ fuctionalprogram/fuctionalprogram/Program.cs
@@ -11,6 +11,7 @@
             Console.WriteLine("4 for a harmonic number");
             Console.WriteLine("5 for a factors");
             Console.WriteLine("6 for a Quotient and Remainder");
+            Console.WriteLine("7 for a Swap Number ");
             int function = Convert.ToInt32(Console.ReadLine());
 
 
@@ -45,6 +46,11 @@
                 case 6:
                     QuotientAndRemainder QuotientRemain = new QuotientAndRemainder();
                     QuotientRemain.CompQuotient();
+                    break;
+
+                case 7:
+                    Swaping_Two_Numbers Swap = new Swaping_Two_Numbers();
+                    Swap.SwapTwoNumber();
                     break;
             }
         }
