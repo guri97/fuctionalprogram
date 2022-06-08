097e0f895b26b65a499764283cfc78460eb61160
guri97 <guri84096@gmail.com>
2022-06-08 20:04:01 +0530

    [gurpreet] Power_of_2

---
+++ fuctionalprogram/fuctionalprogram/Power of 2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fuctionalprogram
+{
+    internal class Power_of_2
+    {
+        public void PowerOfTwo()
+        {
+            Console.WriteLine("please enter a number ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            while (number < 0 || number >= 31)
+            {
+                Console.WriteLine("please enter a valid number: ");
+                number = Convert.ToInt32(Console.ReadLine());
+
+            }
+            for (int i = 1; i <= number; i++)
+            {
+                Console.WriteLine(Math.Pow(2, i));
+
+            }
+        }
+    }
+}
--- fuctionalprogram/fuctionalprogram/Program.cs
+++ fuctionalprogram/fuctionalprogram/Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("Enter a number for relative problem.");
             Console.WriteLine("1 for a flip coin and print percentage of Heads and  Tails");
             Console.WriteLine("2 for a leap year");
+            Console.WriteLine("3 for a power of 2");
             int function = Convert.ToInt32(Console.ReadLine());
 
 
@@ -21,6 +22,11 @@
                 case 2:
                     Leapyear leapYear = new Leapyear();
                     leapYear.Leap_year();
+                    break;
+
+                case 3:
+                    Power_of_2 Power = new Power_of_2();
+                    Power.PowerOfTwo();
                     break;
             }
         }
