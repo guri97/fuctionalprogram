47e62247df22df104b4bc04d8cffe4dd077a47b2
guri97 <guri84096@gmail.com>
2022-06-08 19:49:54 +0530

    [gurpreet] Flipcoin

---
+++ fuctionalprogram/fuctionalprogram/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace fuctionalprogram
+{
+    class functionalprogrm
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number for relative problem.");
+            Console.WriteLine("1 for a flip coin and print percentage of Heads and  Tails");
+            int function = Convert.ToInt32(Console.ReadLine());
+
+
+
+            switch (function)
+            {
+                case 1:
+                    Flipcoin flipCoin = new Flipcoin();
+                    flipCoin.FlipCoin_Percentage();
+                    break;
+            }
+        }
+    }
+}
